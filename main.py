57b6a008315bcb361d2af6e84319ce9b61fbeefe
Dilhanx <dilhanx@outlook.com>
2017-07-29 09:37:51 +0530

    Signed-off-by: Dilhanx <dilhanx@outlook.com>

--- main.py
+++ main.py
@@ -5,5 +5,14 @@ app = Flask(__name__)
 def hello_world():
   return 'Hello, World!'
 
+@app.route('/test')
+def test():
+
+  return 'test'
+@app.route('/test/<test>')
+def testtest(test):
+
+  return test
+
 if __name__ == '__main__':
   app.run()
